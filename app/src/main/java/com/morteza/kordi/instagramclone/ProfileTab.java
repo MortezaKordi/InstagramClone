631c4a49e973e85d8ccfb9bb6c067ceed1730783
wasabeef <morymastercoder@gmail.com>
2018-10-21 05:27:02 -0800

    Implemented the users tab

--- app/src/main/java/com/morteza/kordi/instagramclone/ProfileTab.java
+++ app/src/main/java/com/morteza/kordi/instagramclone/ProfileTab.java
@@ -6,6 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -13,6 +21,10 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+
+    private EditText edtProfileName, edtProfileBio, edtProfileProfession,
+            edtProfileHobbies, edtProfileFavSport;
+    private Button btnUpdateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -23,7 +35,65 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container,
+                false);
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfileFavSport = view.findViewById(R.id.edtProfileFavoriteSport);
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if (parseUser.get("profileName") == null) {
+            edtProfileName.setText("");
+
+        } else {
+            edtProfileName.setText(parseUser.get("profileName").toString());
+
+        }
+
+        edtProfileBio.setText(parseUser.get("profileBio") + "");
+        edtProfileProfession.setText(parseUser.get("profileProfession") + "");
+        edtProfileHobbies.setText(parseUser.get("profileHobbies") + "");
+        edtProfileFavSport.setText(parseUser.get("profileFavSport") + "");
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                parseUser.put("profileName", edtProfileName.getText().toString());
+                parseUser.put("profileBio", edtProfileBio.getText().toString());
+                parseUser.put("profileProfession", edtProfileProfession.getText().toString());
+                parseUser.put("profileHobbies", edtProfileHobbies.getText().toString());
+                parseUser.put("profileFavSport", edtProfileFavSport.getText().toString());
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+
+                            FancyToast.makeText(getContext(),
+                                    "Info Updated",
+                                    Toast.LENGTH_SHORT, FancyToast.INFO,
+                                    true).show();
+
+                        } else {
+                            FancyToast.makeText(getContext(), e.getMessage(),
+                                    Toast.LENGTH_SHORT, FancyToast.ERROR,
+                                    true).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+        return view;
+
     }
 
 }
--- app/src/main/java/com/morteza/kordi/instagramclone/UsersTab.java
+++ app/src/main/java/com/morteza/kordi/instagramclone/UsersTab.java
@@ -6,6 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +24,10 @@ import android.view.ViewGroup;
  */
 public class UsersTab extends Fragment {
 
+
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public UsersTab() {
         // Required empty public constructor
@@ -23,7 +38,41 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e == null) {
+
+                    if (users.size() > 0) {
+
+                        for (ParseUser user : users) {
+
+                            arrayList.add(user.getUsername());
+
+                        }
+
+                        listView.setAdapter(arrayAdapter);
+                        txtLoadingUsers.animate().alpha(0).setDuration(2000);
+                        listView.setVisibility(View.VISIBLE);
+
+                    }
+
+                }
+            }
+        });
+        return view;
+
     }
 
 }
